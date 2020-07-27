0005133af76e9f80f99b9c4ebf031b8818388806
极致 <2590970043@qq.com>
2020-07-27 12:35:35 +0800

    :heavy_plus_sign: Add OPThemeSdk-3.0

--- app/src/main/java/com/jizhi/optheme/MainActivity.kt
+++ app/src/main/java/com/jizhi/optheme/MainActivity.kt
@@ -2,10 +2,21 @@ package com.jizhi.optheme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jeremyliao.liveeventbus.LiveEventBus
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        var i = 0;
+        button.setOnClickListener {
+            LiveEventBus.get("com.jizhi.optheme.sdk\$AodNotification", Int::class.java)
+                .broadcast(editTextTextPersonName.text.toString().toInt(),true,false)
+        }
+        button2.setOnClickListener {
+            LiveEventBus.get("com.jizhi.optheme.sdk\$AodTime", Int::class.java)
+                .broadcast(editTextTextPersonName2.text.toString().toInt(),true,false)
+        }
     }
 }
