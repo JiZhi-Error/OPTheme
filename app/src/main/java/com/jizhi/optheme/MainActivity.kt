d99738c308e26930394bf4d88d56166dd896a942
极致 <2590970043@qq.com>
2020-08-24 22:11:50 +0800

    Add UtilCodex

--- app/src/main/java/com/jizhi/optheme/MainActivity.kt
+++ app/src/main/java/com/jizhi/optheme/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.jizhi.optheme
 
+import android.R.attr
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.blankj.utilcode.util.UriUtils
 import com.jizhi.optheme.sdk.utils.Utils
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,5 +27,23 @@ class MainActivity : AppCompatActivity() {
         button5.setOnClickListener {
             Utils.findProcessAndKill(this)
         }
+//        button6.setOnClickListener {
+//            val intent = Intent(Intent.ACTION_GET_CONTENT)
+//            intent.type = "*/*"
+//            intent.addCategory(Intent.CATEGORY_OPENABLE)
+//            startActivityForResult(intent, 1)
+//        }
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+//        if (resultCode == Activity.RESULT_OK) {
+//            val uri: Uri? = data!!.data
+//            val uri2File = UriUtils.uri2File(uri!!)
+//            println(uri2File)
+//        }
+
+    }
+
 }
